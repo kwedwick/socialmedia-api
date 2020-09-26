9762a5f6981780e85257e00c1a27f0e2dee0c053
KWedwick <64446581+kwedwick@users.noreply.github.com>
2020-09-25 19:04:33 -0500

    modules completed!

---
+++ models/User.js
@@ -0,0 +1,45 @@
+const { Schema, model } = require('mongoose');
+const moment = require('moment');
+
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: 'You must enter a unique username!',
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false
+    }
+);
+
+UserSchema.virtual('friendCount').get(function() {
+    return this.friends.length
+})
+
+const User = model('User', UserSchema);
+
+module.exports = User;
